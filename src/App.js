442fd6492fec485b299ddef52b3b7d3140ef4346
Mickaël Mesloub <114213727+Mickael-Mesloub@users.noreply.github.com>
2023-02-03 08:51:12 +0100

    regex

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react"
 import {Form} from "./components/form/Form"
 import { personnages } from "./components/data/Characters"
 
+// Composant Title : pour titre dynamique en fonction des phases
+
 const App = () => {
 
     const [hero, setHero] = useState(personnages[0]);
@@ -9,9 +11,11 @@ const App = () => {
     const [isFormShown, setIsFormShown] = useState(true);
 
     return(
-
-        <>
+        
+        <> 
             <h1>King of Naheulbeuk</h1>
+
+             {/* Affiche le Form. Si form démonté (onSubmit), affiche message de bienvenue */}
             {isFormShown ? (
                 <Form hero={hero} setHero={setHero}setPseudo={setPseudo} setIsFormShown={setIsFormShown}/>
             ) : 
@@ -26,3 +30,11 @@ const App = () => {
 };
 
 export default App
+
+/* Push sur GitHub:
+
+git add .
+git commit -m "message qu'on veut"
+git push
+
+*/
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,26 +1,72 @@
+import { useEffect, useState } from "react"
 import {personnages} from '../data/Characters'
+
+
+// Pour forcer un nombre minimum de caractères 
+const minLength = 2;
+// Pour forcer une expression rationnelle (interdit les caractères sépciaux sauf espaces (au milieu du pseudo), les tirets et underscores 
+const regex = /^[\p{L}\p{Nd}_-]+[\p{L}\p{Nd}\s_-]+[\p{L}\p{Nd}_-]+$/;
+
+console.log(regex); 
+
 
 export const Form = (props) => {
 
+    const [inputValue, setInputValue] = useState("");
+    const [error, setError] = useState("");
+    
+
+    useEffect(() => {
+        console.log(error)
+    },[error])
+
+
+    const handleChange = (e, value=e.target.value) => {
+
+        setInputValue(value);
+        props.hero.name = value;
+
+        if(inputValue.length < minLength) {
+            setError(`*Veuillez choisir un pseudo d'au moins ${minLength} caractères!`);
+        } else {
+            setError("");
+        }
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        let regex = new RegExp(/^[\p{L}\p{Nd}_-]+[\p{L}\p{Nd}\s_-]+[\p{L}\p{Nd}_-]+$/)
+        // Cache le form onSubmit
         props.setIsFormShown(false);
-        props.setPseudo(props.hero.name)
-    }
-    return(
+        props.setPseudo(props.hero.name);
 
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="hero">Choisissez un personnage </label>
-            <select id="hero" defaultValue={props.hero.id}>
-                {personnages.map((e, i) => { 
-                    return <option  key={i} value={e.id}>{e.breed}</option>
-                })}
-            </select>
-            <br />
-            <label htmlFor="pseudo">Choisissez un pseudo </label>
-            <input onChange={(e) => props.hero.name = e.target.value} type="text" id="pseudo" placeholder="Pseudo" />
-            <input type="submit" value="Let's go !" />
-        </form>
-        
+        // Quand je submit, vérifie si les regex sont respectées. Si non, message d'erreur, caractères spéciaux non autorisés !
+        if (!regex.test(inputValue)) {
+            setError('*Les caractères spéciaux ne sont pas autorisés');
+        } else {
+            setError("");
+        }
+
+        // Quand je submit, hero.breed prend la valeur de l'option choisie
+        console.log(props.hero); 
+     }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="hero">Choisissez un personnage </label>
+                <select id="hero" defaultValue={props.hero.breed}>
+                    {personnages.map((e, i) => { 
+                        return <option  key={i} value={e.id}>{e.breed}</option>
+                    })}
+                </select>
+                <br />
+                <label htmlFor="pseudo">Choisissez un pseudo </label>
+                <input value={inputValue} onChange={handleChange} type="text" id="pseudo" placeholder="Pseudo" />
+                {error && <div style={{color: "red"}}>{error}</div>}
+                <input type="submit" value="Let's go !" />
+            </form>
+        </>
+
     )
 }
